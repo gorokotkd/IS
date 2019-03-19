2d08e83dcf88b6742bfc169a3dc6a07093834f1e
iakigarci <44896401+iakigarci@users.noreply.github.com>
2019-03-19 16:29:57 +0100

    Iñaki funciona

---
+++ IS-master/Proyectois/src/packProyecto/Main.java
@@ -0,0 +1,13 @@
+package packProyecto;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Ratings.getRatings().imprimir(5573);
+		Peliculas.getPeliculas().imprimir(12);;
+		InterfazGrafica interfaz = new InterfazGrafica();
+		BaseDatos.getBd().eliminarBD();
+	}
+
+}
