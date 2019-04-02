2e4c53900ced392eb8abffb6a8e7dfec14a3d6d8
iakigarci <garcianoya@hotmail.es>
2019-04-02 22:22:49 +0200

    Clase similitud

---
+++ Proyectois/src/packProyecto/Similitud.java
@@ -0,0 +1,45 @@
+package packProyecto;
+
+import java.util.ArrayList;
+import java.lang.*;
+
+public class Similitud {
+
+	
+	public Similitud() {
+		
+	}
+	
+	public Double calcularSimilitud(ArrayList<Double> pVPeli1, ArrayList<Double> pVPeli2) {
+		int diff = pVPeli1.size()-pVPeli2.size();
+		if (diff<0) {
+			diff = diff*(-1);
+			for (int i = 0; i < diff; i++) {
+				pVPeli1.add(0.0);
+			}
+		}else if (diff>0) {
+			for (int i = 0; i < diff; i++) {
+				pVPeli2.add(0.0);
+			}
+		}
+		Double sumatorio = this.sumatorio(pVPeli1, pVPeli2);
+		Double modulo = this.modulo(pVPeli1, pVPeli2);
+		return sumatorio/modulo;
+	}
+	
+	public Double sumatorio(ArrayList<Double> pVPeli1, ArrayList<Double> pVPeli2) {
+		Double sumatorio = 0.0;
+		for (int i = 0; i < pVPeli1.size(); i++) {
+			sumatorio = sumatorio + pVPeli1.get(i) + pVPeli2.get(i);
+		}
+		return sumatorio;
+	}
+	
+	public Double modulo(ArrayList<Double> pVPeli1, ArrayList<Double> pVPeli2) {
+		Double rdo = 0.0;
+		for (int i = 0; i < pVPeli1.size(); i++) {
+			rdo = Math.sqrt(Math.pow(pVPeli1.get(i),2.0)) + Math.sqrt(Math.pow(pVPeli2.get(i),2.0)); 
+		}
+		return rdo;
+	}
+}
